2443658b6a8bda232a02d9fd04ae0f14cfe97284
CloningApps <collabvm.akka@gmail.com>
2024-12-07 12:41:21 +0100

    Create screen.c

---
+++ kernel/screen.c
@@ -0,0 +1,14 @@
+#include "screen.h"
+
+void init_screen() {
+    // Screen initialization code
+}
+
+void print(const char* message) {
+    // Print message to screen
+}
+
+void clear_screen() {
+    // Clear screen code
+}
+
