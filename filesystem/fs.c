6d58518547bb2f407fc50afa806f0aa3051839d7
CloningApps <collabvm.akka@gmail.com>
2024-12-07 12:37:57 +0100

    Create fs.c

---
+++ filesystem/fs.c
@@ -0,0 +1,14 @@
+#include "fs.h"
+
+void init_filesystem() {
+    // Filesystem initialization code
+}
+
+void read_file(const char* filename) {
+    // Read file code
+}
+
+void write_file(const char* filename, const char* content) {
+    // Write file code
+}
+
